a0385f0108559c57e7f2bd866bcb2227570d6dd2
André Pantoja De Souza <111390846+Andrepj10@users.noreply.github.com>
2023-07-25 13:07:53 -0400

    logout funcionando

--- agenda/views.py
+++ agenda/views.py
@@ -1,18 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+
+from django.contrib import messages
 
 from agenda.models import Fotografia
 
 #Responsavel por mostrar o conteudo das telas
 def index(request):
-    #se eu por um - antes de data_fotografia aparecerá no index a antiga primeiro.
-    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
-    return render(request, 'agenda/index.html',{"cards": fotografias})
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+
+    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+    return render(request, 'agenda/index.html', {"cards": fotografias})
+
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'agenda/imagem.html', {"fotografia": fotografia})
 
 def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+    
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
     if "buscar" in request.GET:
         nome_a_buscar = request.GET['buscar']
@@ -22,16 +32,32 @@ def buscar(request):
     return render (request, "agenda/buscar.html", {"cards":fotografias})
 
 def anotacoes(request):
-    return render(request, 'agenda/anotacoes.html')
+     if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+    
+     return render(request, 'agenda/anotacoes.html')
 
 
 def metas(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+    
     return render(request, 'agenda/metas.html')
 
 
 def tarefas(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+    
     return render(request, 'agenda/tarefas.html')
 
 
 def eventos(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+    
     return render(request, 'agenda/eventos.html')
